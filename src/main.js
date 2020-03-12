2a9914465a04122553100d2f6ad86ed2b0c83171
Pitt Wu <kgb00128@gmail.com>
2020-03-12 22:02:14 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+
+// styles
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/scss/style.scss'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
